ad5d4512eebe5318aac8b6e5bae15e0c0f43854b
Timur Rubeko <trubeko@kyriba.com>
2016-04-10 19:17:34 +0200

    An attempt to get issue names from branch name. Abondoned in favor of reflog

--- src/Git.hs
+++ src/Git.hs
@@ -12,59 +12,108 @@ import Data.Time.Format
 import Text.Regex.Posix
 import Data.Time
 import qualified Data.List as L
+import Data.List.Split
 
 import Config
 import Util
 import Jira
 
 
+type Sha1 = String
+type Message = String
 type User = String
 type GitArgs = [String]
 
-data Commit = Commit Day Issue deriving (Show, Eq, Ord)
+data Commit = Commit Sha1 Day Message deriving (Show, Eq)
+data RelevantCommit = RelevantCommit Day Issue deriving (Show, Eq, Ord)
 
 
 getGitUser :: IO User
 getGitUser = trim <$> readProcess "git" ["config", "user.email"] []
 
 
-getGitLog :: Config -> User -> [String] -> IO [Commit]
-getGitLog config user args = do
-  let gitArgs = ["log", "--format=%aI %s", "--author="++user] ++ args
-  log <- getGitLogLines user gitArgs (getGitRepositories config)
-  return $ extractCommits log (getIssuePatterns config)
+getGitLog :: Config -> User -> [String] -> IO [RelevantCommit]
+getGitLog config user args = concat <$> mapM (getGitLog' config user args) (getGitRepositories config)
 
+getGitLog' :: Config -> User -> [String] -> FilePath -> IO [RelevantCommit]
+getGitLog' config user args repo = do
+  log <- gitLog' user args repo
+  let issuePatterns = getIssuePatterns config
+      byMessages = fromMessages log issuePatterns
+  byBranches <- fromBranches log repo issuePatterns
+  return $ byMessages ++ byBranches
+
+fromBranches :: [Commit] -> FilePath -> [IssuePattern] -> IO [RelevantCommit]
+fromBranches log repo issuePatterns =
+  concat <$> mapM (fromBranches' repo issuePatterns) log
+
+
+
+
+
+--- TODO: finally it looks like I took a wrong approach; if all done locally -
+--  can reliably know a branch name and a message for any commit, from reflog
+--  drawback - only locally (is it?)
+--  see stackoverflow.com/questions/2706797/finding-what-branch-a-git-commit-came-from
+--  second idea in first answer
+
+
+
+
+fromBranches' :: FilePath -> [IssuePattern] -> Commit -> IO [RelevantCommit]
+fromBranches' repo issuePatterns (Commit sha1 day _) = do
+  let args = ["branch", "-a", "--contains="++sha1]
+      gitProcess = (proc "git" args){cwd = Just repo}
+  allBranches <- map (drop 2) . lines <$> readCreateProcess gitProcess ""
+  let onlyNames = map (last . splitOn "/") allBranches
+      branches = L.nub onlyNames
+  case branches of
+    [] -> return []
+    _ -> do
+           branch <- earliestBranch branches
+           return $ map (RelevantCommit day) $ findIssues branch issuePatterns
+
+earliestBranch :: [String] -> IO String
+earliestBranch bs = do
+  allBranches <- allBranchesByCreation
+  return $ head $ dropWhile (`elem` bs) allBranches
+
+allBranchesByCreation :: IO [String]
+allBranchesByCreation = return [] -- TODO: list all branches in chronological order
+
+-- Note: doesn't attempt to match same pattern several times (TODO: match multiple times?)
+findIssues :: String -> [IssuePattern] -> [Issue]
+findIssues xs issuePatterns = filter (not . null) $ map (xs =~) issuePatterns
 
 -- extract commits matching any expected issue patter from git log
-extractCommits :: [String] -> [IssuePattern] -> [Commit]
-extractCommits log = concatMap (extractCommits' log)
-
--- extract commits mathcing expected issue pattern from git log
-extractCommits' :: [String] -> IssuePattern -> [Commit]
-extractCommits' log issuePattern =
-  let relevant = filter (=~ issuePattern) log
-      issues = map (=~ issuePattern) relevant
-      dates = map (parseTimeOrError False defaultTimeLocale "%Y-%m-%d" . take 10) relevant
-  in zipWith Commit dates issues
+fromMessages :: [Commit] -> [IssuePattern] -> [RelevantCommit]
+fromMessages log issuePatterns =
+  concatMap (\(Commit _ d m) -> map (RelevantCommit d) (findIssues m issuePatterns)) log
 
 
--- extract git log lines for all given repositories, merged to one list
-getGitLogLines :: User -> GitArgs -> [FilePath] -> IO [String]
-getGitLogLines user gitArgs repos = let listOfLogs = mapM (getGitLogLines' user gitArgs) repos
-                                    in  concat <$> listOfLogs
+-- extract git log lines for a repository
+gitLog' :: User -> GitArgs -> FilePath -> IO [Commit]
+gitLog' user gitArgs repoPath = do
+  let args = ["log", "--format=%H%ai%s", "--author="++user] ++ gitArgs
+      gitProcess = (proc "git" args){cwd = Just repoPath}
+  logLines <- lines <$> readCreateProcess gitProcess ""
+  return $ map parseLogLine logLines
 
--- extract git log lines for one repository only
-getGitLogLines' :: User -> GitArgs -> FilePath -> IO [String]
-getGitLogLines' user gitArgs repoPath = do
-  let args = ["log", "--format=%aI %s", "--author="++user] ++ gitArgs
-      gitProcess = (proc "git" gitArgs){cwd = Just repoPath}
-  lines <$> readCreateProcess gitProcess ""
+-- parses log lines from `git log` in fomrat `%H%ai%s` into a `Commit`
+-- TODO: splitOn
+parseLogLine :: String -> Commit
+parseLogLine s =
+  let sha1   = take 40 s -- sha1 == 40ch
+      dayStr = take 10 (drop 40 s) -- date == 10ch after sha1
+      day    = parseTimeOrError False defaultTimeLocale "%Y-%m-%d" dayStr
+      msg    = drop 65 s -- message == rest after sha1+time(25ch)
+  in  Commit sha1 day msg
 
 
-calculateWorkLog :: [Commit] -> [WorkLog]
-calculateWorkLog gitLog =
-  let noDuplicates = L.nub gitLog
-      grouped = L.groupBy (\(Commit d1 _) (Commit d2 _) -> d1 == d2) $ L.sort noDuplicates
+calculateWorkLog :: [RelevantCommit] -> [WorkLog]
+calculateWorkLog log =
+  let noDuplicates = L.nub log
+      grouped = L.groupBy (\(RelevantCommit d1 _) (RelevantCommit d2 _) -> d1 == d2) $ L.sort noDuplicates
       avHours = map (\cs -> 8.0 / (fromIntegral . length $ cs)) grouped
-      logs = zipWith (\cs h -> map (\(Commit d i) -> WorkLog d i h) cs) grouped avHours
+      logs = zipWith (\cs h -> map (\(RelevantCommit d i) -> WorkLog d i h) cs) grouped avHours
   in  concat logs
